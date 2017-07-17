67a494429dd28d5591812d91ccab1b4286dd5b3e
Nathan Bolam <bolamnathan@gmail.com>
2017-07-17 18:44:02 +0930

    satisfy strong-remoting

--- index.js
+++ index.js
@@ -1,32 +1,71 @@
-var mongodb = require('mongodb');
+var mongodb;
 
-module.exports = function(app) {
+mongodb = require('mongodb');
 
-  var ObjectID = function(id) {
+module.exports = function(arg) {
+  var ObjectID, registry, remotes;
+  registry = arg.registry, remotes = arg.remotes;
+  ObjectID = function(id) {
+    var e;
     if (!id) {
       return new mongodb.ObjectID();
     }
-
     if (id instanceof mongodb.ObjectID) {
       return id;
     }
-
     if (typeof id !== 'string') {
       return id;
     }
-
     try {
       if (/^[0-9a-fA-F]{24}$/.test(id)) {
         return new mongodb.ObjectID(id);
       } else {
         return id;
       }
-    } catch (error) { 
+    } catch (error1) {
+      e = error1;
       return id;
     }
-
   };
-
-  app.registry.modelBuilder.defineValueType(ObjectID);
-
+  registry.modelBuilder.defineValueType(ObjectID);
+  remotes().defineType('ObjectId', {
+    fromTypedValue: function(ctx, value) {
+      var error;
+      if (value == null) {
+        return {
+          value: value
+        };
+      }
+      value = new ObjectID(value);
+      error = this.validate(ctx, value);
+      if (error) {
+        return {
+          error: error
+        };
+      } else {
+        return {
+          value: value
+        };
+      }
+    },
+    fromSloppyValue: function(ctx, value) {
+      var objectConverter, result;
+      objectConverter = ctx.typeRegistry.getConverter('object');
+      result = objectConverter.fromSloppyString(ctx, value);
+      if (result.error) {
+        return result;
+      } else {
+        return this.fromTypedValue(ctx, result.value);
+      }
+    },
+    validate: function(ctx, value) {
+      if (value == null) {
+        return null;
+      }
+      if (typeof value !== 'object' || !(value instanceof ObjectID)) {
+        return new Error('Value is not an instance of ObjectID.');
+      }
+      return null;
+    }
+  });
 };
--- src/index.coffee
+++ src/index.coffee
@@ -1,6 +1,6 @@
 mongodb = require 'mongodb'
 
-module.exports = (app) ->
+module.exports = ({ registry, remotes }) ->
 
   ObjectID = (id) ->
     if not id
@@ -17,11 +17,42 @@ module.exports = (app) ->
         return new mongodb.ObjectID(id)
       else
         return id
-    catch
+    catch e
       return id
 
     return
 
-  app.registry.modelBuilder.defineValueType ObjectID
+  registry.modelBuilder.defineValueType ObjectID
+
+  remotes().defineType 'ObjectId',
+    fromTypedValue: (ctx, value) ->
+      if not value?
+        return { value }
+
+      value = new ObjectID value
+      error = @validate ctx, value
+
+      if error
+        { error }
+      else 
+        { value }
+
+    fromSloppyValue: (ctx, value) ->
+      objectConverter = ctx.typeRegistry.getConverter 'object'
+      result = objectConverter.fromSloppyString ctx, value
+
+      if result.error 
+        result 
+      else 
+        @fromTypedValue ctx, result.value
+
+    validate: (ctx, value) ->
+      if not value?
+        return null
+
+      if typeof value isnt 'object' or !(value instanceof ObjectID)
+        return new Error 'Value is not an instance of ObjectID.'
+
+      null
 
   return
